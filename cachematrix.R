4d6d74e70d72905e04a75feb4da62f0d9b27802c
snigavik1982 <snigavik@gmail.com>
2016-03-19 16:44:31 +0300

    cleaned

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -22,7 +19,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   m <- x$getInverse()
   if (!is.na(m)) {
     message("getting cached data")
